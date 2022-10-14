3644ddea90599f6c087c7febc5ea615c0c19359e
bagas_wap <bagas_wap@outlook.com>
2022-10-14 16:59:00 +0700

    list handle

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,8 +2,8 @@ import React from 'react'
 import Person from './Person'
 
 function NameList() {
-    // const names = ['Bruce', 'Clark', 'Diana']
-    // const namesList = names.map(name => <Person name={name}></Person>)
+    const names = ['Bruce', 'Clark', 'Diana']
+    const namesList = names.map((name, index) => <h2 key={index}>{name}</h2>)
     const persons = [
         {
             id: 1,
@@ -23,11 +23,11 @@ function NameList() {
             age: 26,
             skill: 'angular'
         }]
-    const personsList = persons.map(person => <Person person={person}></Person>)
+    // const personsList = persons.map(person => <Person key={person.id} person={person}></Person>)
     return (
     <div>
-        {/* <div>{namesList}</div> */}
-        <div>{personsList}</div>
+        <div>{namesList}</div>
+        {/* <div>{personsList}</div> */}
     </div>
     )
 }
